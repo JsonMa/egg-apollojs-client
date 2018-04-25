9eda56760068465ee21caff44a45dd7450b111dc
奕豪杰 <haojie.yi@downtown8.com>
2018-04-25 18:03:30 +0800

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict';
 
-const apollo = require('./lib/apollo')
+const apollo = require('./lib/apollo');
 
-module.exports = (path) => {
+exports.apollo = apollo;
+
+exports.init = path => {
   const apolloConfig = require(path);
 
   // 读取携程apollo配置中心，并创建default.env文件
   apollo.remoteConfigServiceSikpCache(apolloConfig).then(bundle => {
-    console.log('Config From Apollo:'.green, '\n', bundle);
+    console.log('Config From Apollo:', '\n', bundle);
     apollo.createEnvFile(bundle);
+    apollo.setEnv();
   }).catch(err => {
     console.error('Error From Apollo:'.red, err);
   }).done;
 
-}
+};
