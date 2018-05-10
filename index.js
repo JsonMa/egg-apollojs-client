00dc425c83577a96bcb792579efe313897b529f4
奕豪杰 <haojie.yi@downtown8.com>
2018-05-10 17:08:19 +0800

    fix bug

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ exports.init = path => {
   // 读取携程apollo配置中心，并创建default.env文件
   apollo.remoteConfigServiceSikpCache(apolloConfig).then(bundle => {
     console.log('Config From Apollo:', '\n', bundle);
+    Object.assign(bundle, apolloConfig);
     apollo.createEnvFile(bundle);
     apollo.setEnv();
   }).catch(err => {
